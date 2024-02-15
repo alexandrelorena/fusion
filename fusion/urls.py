7312057f2f959ba375b6efa26d8158a4a1e3250a
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-02-14 23:24:24 -0300

    Atualização

--- fusion/urls.py
+++ fusion/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('!@painel/', admin.site.urls),
     path('', include('core.urls')),
     path('pictures/', include('pictures.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
